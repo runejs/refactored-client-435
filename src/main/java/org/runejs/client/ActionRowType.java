999def80fc61053d2762de4c51eaa6669ce25f5e
James Monger <jameskmonger@hotmail.co.uk>
2022-09-08 08:53:20 +0100

    docs: notes

--- src/main/java/org/runejs/client/ActionRowType.java
+++ src/main/java/org/runejs/client/ActionRowType.java
@@ -7,6 +7,47 @@ package org.runejs.client;
  * @author jameskmonger
  */
 public enum ActionRowType {
+    // 23, 57, 54 are all some kind of interface click
+    //     they do different things with the varps
+    //     23 is toggle?
+    //     57 ????? reads from alternateRhs and switches varp
+    //     54 is click here to continue
+
+    // 9 and 40 are interface close
+    //      9 appears to be the longstanding chatbox dialogue close
+    //      40 is anything else
+
+    // 42 is some kind of tooltip. 
+    //          Sends packet 64 (button click) to server and does further processing
+
+    // the ITEM_ON_WIDGET_INTERACTION are for 'new format' widgets
+    // 1007 is examine item on new format widget
+
+    // 53, 25, 55, 48, 24 are options on old format widget
+    // 1006 is examine item on old format widget
+
+    // 12 is NPC interaction
+    // 4 is also NPC interaction
+    
+    // 2 is ??
+    // 3 is pickup item (specific? or is it 1st item option)
+    // 44 is ??
+    // 16 is ??
+    // 17 is ??
+    // 20 is ??
+    // 50 is ??
+    // 225 is ?? (bit shifts the target by 14..)
+    
+    // 187 is ???? interacts with player in some way
+    // 39 is second option on player (what is this? follow?)
+    
+    // 8 interacts with worlditem (e.g. light logs)
+    // 54 is 'click to continue' on dialogue
+    // 36 is ??
+
+    // 47 is item on world item
+
+
     MAGIC_ON_PLAYER(1),
     ITEM_ON_WIDGET_INTERACTION_OPTION_2(6),
     DROP_ITEM(11),
