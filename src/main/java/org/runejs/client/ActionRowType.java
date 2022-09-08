2aa1eff1032c5d030f1a50cabd08a8d5573418b8
James Monger <jameskmonger@hotmail.co.uk>
2022-09-08 20:17:22 +0100

    docs: add docs on all properties

--- src/main/java/org/runejs/client/ActionRowType.java
+++ src/main/java/org/runejs/client/ActionRowType.java
@@ -7,6 +7,8 @@ package org.runejs.client;
  * @author jameskmonger
  */
 public enum ActionRowType {
+    // missing ActionRowTypes:
+
     // 42
     //     interface
 
@@ -21,6 +23,8 @@ public enum ActionRowType {
 
     // 54
     //      click to continue? (what else?)
+
+    // 1005 has some significance, what is it?
 
     /**
      * Toggle the value of a varp.
@@ -37,19 +41,60 @@ public enum ActionRowType {
      */
     BUTTON_SET_VARP_VALUE(57),
 
+    /**
+     * Walk to a tile.
+     */
     WALK_HERE(7),
+
+    /**
+     * Drop an item from the inventory.
+     */
     DROP_ITEM(11),
 
+    /**
+     * Send a private message to someone on the player's friends list.
+     */
     MESSAGE_FRIEND(18),
+
+    /**
+     * Add someone to the player's friends list.
+     */
     ADD_FRIEND(45),
+
+    /**
+     * Remove someone from the player's friends list.
+     */
     REMOVE_FRIEND(13),
+
+    /**
+     * Add someone to the player's ignore list.
+     */
     ADD_IGNORE(51),
+
+    /**
+     * Remove someone from the player's ignore list.
+     */
     REMOVE_IGNORE(35),
+
+    /**
+     * Accept a trade request.
+     */
     ACCEPT_TRADE(26),
+
+    /**
+     * Accept a duel request.
+     */
     ACCEPT_CHALLENGE(46),
+
+    /**
+     * Report a player for abuse.
+     */
     REPORT_ABUSE(28),
+    
     /**
      * The initial 'use' to select an item on a widget
+     * 
+     * TODO TODO TODO is this inventory or widget ??
      */
     SELECT_ITEM_ON_WIDGET(19),
 
@@ -58,127 +103,171 @@ public enum ActionRowType {
      */
     SELECT_SPELL_ON_WIDGET(33),
 
+    /**
+     * Use an item on another player.
+     */
     USE_ITEM_ON_PLAYER(22),
 
+    /**
+     * Use an item on an NPC.
+     */
     USE_ITEM_ON_NPC(49),
 
     /**
-     * For when the player uses an inventory item on another.
+     * Use an item on an inventory item.
      *
      * TODO TODO TODO is this inventory or widget ??
      */
     USE_ITEM_ON_INVENTORY_ITEM(56),
 
     /**
-     * Player uses an item on a world item
+     * Use an item on a world item (i.e. an item on the ground)
      */
     USE_ITEM_ON_WORLD_ITEM(47),
 
     /**
-     * Player uses an item on an object
+     * Use an item on an object.
      */
     USE_ITEM_ON_OBJECT(5),
 
+    /**
+     * Cast a spell on a player.
+     */
     CAST_MAGIC_ON_PLAYER(1),
+
+    /**
+     * Cast a spell on an NPC.
+     */
     CAST_MAGIC_ON_NPC(21),
+
+    /**
+     * Cast a spell on a widget item.
+     * 
+     * TODO TODO TODO inventory or widget?
+     */
     CAST_MAGIC_ON_WIDGET_ITEM(37),
+
+    /**
+     * Cast a spell on a world item (i.e. an item on the ground)
+     */
     CAST_MAGIC_ON_WORLD_ITEM(15),
+
+    /**
+     * Cast a spell on an object.
+     */
     CAST_MAGIC_ON_OBJECT(32),
 
     /**
-     * The first Player interaction option
+     * Interact with a player and choose option 1.
      */
     INTERACT_WITH_PLAYER_OPTION_1(10),
+    
     /**
-     * The second Player interaction option
+     * Interact with a player and choose option 2.
      */
     INTERACT_WITH_PLAYER_OPTION_2(39),
+    
     /**
-     * The third Player interaction option
+     * Interact with a player and choose option 3.
      */
     INTERACT_WITH_PLAYER_OPTION_3(44),
+    
     /**
-     * The fourth Player interaction option
+     * Interact with a player and choose option 4.
      */
     INTERACT_WITH_PLAYER_OPTION_4(14),
+    
     /**
-     * The fifth Player interaction option
+     * Interact with a player and choose option 5.
      */
     INTERACT_WITH_PLAYER_OPTION_5(41),
 
+    
     /**
-     * The first NPC interaction option
+     * Interact with an NPC and choose option 1.
      *
      * observed values: [ 'Talk to' ]
      */
     INTERACT_WITH_NPC_OPTION_1(12),
+
     /**
-     * The second NPC interaction option
+     * Interact with an NPC and choose option 2.
      */
     INTERACT_WITH_NPC_OPTION_2(30),
+
     /**
-     * The third NPC interaction option
+     * Interact with an NPC and choose option 3.
      *
      * observed values: [ 'Trade' ]
      */
     INTERACT_WITH_NPC_OPTION_3(4),
+
     /**
-     * The fourth NPC interaction option
+     * Interact with an NPC and choose option 4.
      */
     INTERACT_WITH_NPC_OPTION_4(34),
+
     /**
-     * The fifth NPC interaction option
+     * Interact with an NPC and choose option 5.
      */
     INTERACT_WITH_NPC_OPTION_5(20),
 
     /**
-     * The first object interaction option
+     * Interact with an object and choose option 1.
      */
     INTERACT_WITH_OBJECT_OPTION_1(16),
+    
     /**
-     * The second object interaction option
+     * Interact with an object and choose option 2.
      */
     INTERACT_WITH_OBJECT_OPTION_2(29),
+    
     /**
-     * The third object interaction option
+     * Interact with an object and choose option 3.
      */
     INTERACT_WITH_OBJECT_OPTION_3(17),
+    
     /**
-     * The fourth object interaction option
+     * Interact with an object and choose option 4.
      */
     INTERACT_WITH_OBJECT_OPTION_4(27),
+
     /**
-     * The fifth object interaction option
+     * Interact with an object and choose option 5.
      *
      * (jameskmonger) why is this so high? it sits with the Examine ids
      */
     INTERACT_WITH_OBJECT_OPTION_5(1002),
 
     /**
-     * The first world item interaction option
+     * Interact with a world item and choose option 1.
      */
     INTERACT_WITH_WORLD_ITEM_OPTION_1(2),
+
     /**
-     * The second world item interaction option
+     * Interact with a world item and choose option 2.
      */
     INTERACT_WITH_WORLD_ITEM_OPTION_2(38),
+    
     /**
-     * The third world item interaction option
+     * Interact with a world item and choose option 3.
      */
     INTERACT_WITH_WORLD_ITEM_OPTION_3(3),
+    
     /**
-     * The fourth world item interaction option
+     * Interact with a world item and choose option 4.
      */
     INTERACT_WITH_WORLD_ITEM_OPTION_4(8),
+    
     /**
-     * The fifth world item interaction option
+     * Interact with a world item and choose option 5.
      */
     INTERACT_WITH_WORLD_ITEM_OPTION_5(36),
 
     /**
-     * Used for the 'interact' options on an item on a V1 (old format) interface
-     *
-     * For example, these are the 5 options when you right-click an item in a shop.
+     * Interact with an item on a V1 widget and choose option 1.
+     * 
+     * For example, the left-click option on an item on a shop interface.
      */
     INTERACT_WITH_ITEM_ON_V1_WIDGET_OPTION_1(53),
     INTERACT_WITH_ITEM_ON_V1_WIDGET_OPTION_2(25),
@@ -187,19 +276,50 @@ public enum ActionRowType {
     INTERACT_WITH_ITEM_ON_V1_WIDGET_OPTION_5(24),
 
     /**
-     * Used for the 'interact' options on an item on a V2 (new format) interface
+     * Interact with an item on a V1 widget and choose option 2.
      */
     INTERACT_WITH_ITEM_ON_V2_WIDGET_OPTION_1(52),
-    // this one is read on the server as 'option 5' - which is correct?
+    
+    /**
+     * Interact with an item on a V1 widget and choose option 3.
+     * 
+     * (jameskmongeR) this one is read on the server as 'option 5' - which is correct?
+     */
     INTERACT_WITH_ITEM_ON_V2_WIDGET_OPTION_2(6),
+
+    /**
+     * Interact with an item on a V1 widget and choose option 3.
+     */
     INTERACT_WITH_ITEM_ON_V2_WIDGET_OPTION_3(31),
+    
+    /**
+     * Interact with an item on a V1 widget and choose option 4.
+     */
     INTERACT_WITH_ITEM_ON_V2_WIDGET_OPTION_4(43),
 
+    /**
+     * Examine an NPC.
+     */
     EXAMINE_NPC(1001),
+    
+    /**
+     * Examine an item.
+     */
     EXAMINE_ITEM(1003),
+
+    /**
+     * Examine an object.
+     */
     EXAMINE_OBJECT(1004),
-    // TODO (jameskmonger) 1005 has some significance, what is it?
+
+    /**
+     * Examine an item on a V1 widget.
+     */
     EXAMINE_ITEM_ON_V1_WIDGET(1006),
+
+    /**
+     * Examine an item on a V2 widget.
+     */
     EXAMINE_ITEM_ON_V2_WIDGET(1007);
 
     private final int id;
