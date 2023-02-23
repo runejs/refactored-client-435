5c1b0ecc541e17a42687c621f5b543ec994e78c4
James Monger <jameskmonger@hotmail.co.uk>
2023-02-23 23:50:50 +0000

    feat: set up handlers + codec

---
+++ src/main/java/org/runejs/client/message/handler/rs435/RS435HandlerRegistry.java
@@ -0,0 +1,8 @@
+package org.runejs.client.message.handler.rs435;
+
+import org.runejs.client.message.handler.MessageHandlerRegistry;
+
+public class RS435HandlerRegistry extends MessageHandlerRegistry {
+    public RS435HandlerRegistry() {
+    }
+}
---
+++ src/main/java/org/runejs/client/net/codec/runejs435/RuneJS435PacketCodec.java
@@ -0,0 +1,22 @@
+package org.runejs.client.net.codec.runejs435;
+
+import org.runejs.client.net.PacketType;
+import org.runejs.client.net.codec.MessagePacketCodec;
+
+/**
+ * A {@link MessagePacketCodec} for the RuneJS customised 435 protocol.
+ */
+public class RuneJS435PacketCodec extends MessagePacketCodec {
+    public RuneJS435PacketCodec() {
+        super(PacketType.incomingPacketSizes);
+
+        this.registerEncoders();
+        this.registerDecoders();
+    }
+
+    private void registerEncoders() {
+    }
+
+    private void registerDecoders() {
+    }
+}
