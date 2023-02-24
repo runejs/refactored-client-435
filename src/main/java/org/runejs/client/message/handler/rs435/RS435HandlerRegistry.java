c445ab7b5cf086d9c1bcaf297b7cde5338658bca
James Monger <jameskmonger@hotmail.co.uk>
2023-02-24 18:12:50 +0000

    docs: add docs on creating new messages

--- src/main/java/org/runejs/client/message/handler/rs435/RS435HandlerRegistry.java
+++ src/main/java/org/runejs/client/message/handler/rs435/RS435HandlerRegistry.java
@@ -2,6 +2,9 @@ package org.runejs.client.message.handler.rs435;
 
 import org.runejs.client.message.handler.MessageHandlerRegistry;
 
+/**
+ * A {@link MessageHandlerRegistry} for the RS revision 435 client.
+ */
 public class RS435HandlerRegistry extends MessageHandlerRegistry {
     public RS435HandlerRegistry() {
     }
