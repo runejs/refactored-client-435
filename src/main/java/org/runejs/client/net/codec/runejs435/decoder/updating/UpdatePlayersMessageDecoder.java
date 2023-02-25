9f3d709e75f833d566a88d4e2725d9f299683c6f
James Monger <jameskmonger@hotmail.co.uk>
2023-02-25 13:49:12 +0000

    feat: decode `OtherPlayersMovementUpdate`
    If you want to see the original implementation to compare, look at
    `Player.parseTrackedPlayerMovement`.

--- src/main/java/org/runejs/client/net/codec/runejs435/decoder/updating/UpdatePlayersMessageDecoder.java
+++ src/main/java/org/runejs/client/net/codec/runejs435/decoder/updating/UpdatePlayersMessageDecoder.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.runejs.client.message.inbound.updating.LocalPlayerMovementUpdate;
+import org.runejs.client.message.inbound.updating.OtherPlayersMovementUpdate;
 import org.runejs.client.message.inbound.updating.PlayerMovementUpdate;
 import org.runejs.client.message.inbound.updating.UpdatePlayersInboundMessage;
 import org.runejs.client.message.inbound.updating.LocalPlayerMovementUpdate.LocalPlayerMapRegionChangeUpdate;
+import org.runejs.client.message.inbound.updating.OtherPlayersMovementUpdate.OtherPlayerMovementUpdate;
 import org.runejs.client.net.PacketBuffer;
 import org.runejs.client.net.codec.MessageDecoder;
 
@@ -26,11 +28,12 @@ public class UpdatePlayersMessageDecoder implements MessageDecoder<UpdatePlayers
     public UpdatePlayersInboundMessage decode(PacketBuffer buffer) {
         buffer.initBitAccess();
         LocalPlayerMovementUpdate localPlayerMovement = decodeLocalPlayerMovementUpdate(buffer);
+        OtherPlayersMovementUpdate otherPlayersMovement = decodeOtherPlayersMovementUpdate(buffer);
         buffer.finishBitAccess();
 
         return new UpdatePlayersInboundMessage(
                 localPlayerMovement,
-                null,
+                otherPlayersMovement,
                 null,
                 null);
     }
@@ -94,5 +97,65 @@ public class UpdatePlayersMessageDecoder implements MessageDecoder<UpdatePlayers
         throw new IllegalStateException("Invalid movement type: " + movementType);
     }
 
+    /**
+     * Decodes the movement details of other players into an {@link OtherPlayersMovementUpdate}.
+     * 
+     * @param buffer The buffer.
+     * @return The other players movement update.
+     */
+    private OtherPlayersMovementUpdate decodeOtherPlayersMovementUpdate(PacketBuffer buffer) {
+        int trackedPlayerCount = buffer.getBits(8);
+
+        List<OtherPlayerMovementUpdate> updates = new ArrayList<OtherPlayerMovementUpdate>();
+
+        for (int i = 0; trackedPlayerCount > i; i++) {
+            boolean updateRequired = buffer.getBits(1) == 1;
+
+            // No update required
+            if (!updateRequired) {
+                updates.add(null);
+                continue;
+            }
+
+            int movementType = buffer.getBits(2);
+
+            // No movement
+            if (movementType == 0) {
+                updates.add(new OtherPlayerMovementUpdate(null, false));
+                continue;
+            }
+
+            // deregister
+            if (movementType == 3) {
+                updates.add(new OtherPlayerMovementUpdate(null, true));
+                continue;
+            }
+
+            // walking
+            if (movementType == 1) {
+                int walkDirection = buffer.getBits(3);
+                boolean runUpdateBlock = buffer.getBits(1) == 1;
+
+                updates.add(new OtherPlayerMovementUpdate(
+                    new PlayerMovementUpdate(walkDirection, null, runUpdateBlock), false));
+                continue;
+            }
+
+            // running
+            if (movementType == 2) {
+                int walkDirection = buffer.getBits(3);
+                int runDirection = buffer.getBits(3);
+                boolean runUpdateBlock = buffer.getBits(1) == 1;
+
+                updates.add(new OtherPlayerMovementUpdate(
+                    new PlayerMovementUpdate(walkDirection, runDirection, runUpdateBlock), false));
+                continue;
+            }
+
+            throw new IllegalStateException("Invalid movement type: " + movementType);
+        }
+
+        return new OtherPlayersMovementUpdate(trackedPlayerCount, updates.toArray(new OtherPlayerMovementUpdate[updates.size()]));
+    }
 
 }
