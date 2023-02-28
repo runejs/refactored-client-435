6920d6cd0beea7c18d1f44448233760d1bc030fa
James Monger <jameskmonger@hotmail.co.uk>
2023-02-28 19:04:26 +0000

    feat: handle other player placement/movement
    You can see the original implementation at
    `Player.parseTrackedPlayerMovement`, for reference.

--- src/main/java/org/runejs/client/message/handler/rs435/UpdatePlayersMessageHandler.java
+++ src/main/java/org/runejs/client/message/handler/rs435/UpdatePlayersMessageHandler.java
@@ -1,11 +1,15 @@
 package org.runejs.client.message.handler.rs435;
 
 import org.runejs.client.Class17;
+import org.runejs.client.MovedStatics;
 import org.runejs.client.media.renderable.actor.Actor;
 import org.runejs.client.media.renderable.actor.Player;
 import org.runejs.client.message.handler.MessageHandler;
 import org.runejs.client.message.inbound.updating.LocalPlayerMovementUpdate;
+import org.runejs.client.message.inbound.updating.OtherPlayersMovementUpdate;
+import org.runejs.client.message.inbound.updating.PlayerMovementUpdate;
 import org.runejs.client.message.inbound.updating.UpdatePlayersInboundMessage;
+import org.runejs.client.message.inbound.updating.OtherPlayersMovementUpdate.OtherPlayerMovementUpdate;
 
 /**
  * Applies the main "player updating" message to the game state.
@@ -23,7 +27,10 @@ public class UpdatePlayersMessageHandler implements MessageHandler<UpdatePlayers
         
         // handle the different types of update within the message
         handleLocalPlayerMovement(message.localPlayerMovement);
+        handleOtherPlayersMovement(message.otherPlayersMovement);
 
+        // TODO handle others
+        
         // handle any deregistrations
         for(int i = 0; Class17.deregisterActorCount > i; i++) {
             int trackedPlayerIndex = Player.deregisterActorIndices[i];
@@ -42,7 +49,7 @@ public class UpdatePlayersMessageHandler implements MessageHandler<UpdatePlayers
     /**
      * Responsible for applying movement to the client's local player.
      * 
-     * @param update The update method
+     * @param update The update information to apply
      */
     private void handleLocalPlayerMovement(LocalPlayerMovementUpdate update) {
         // if there is no update at all we do nothing
@@ -91,4 +98,70 @@ public class UpdatePlayersMessageHandler implements MessageHandler<UpdatePlayers
         // code should never reach here based on if statements above
         throw new RuntimeException("Unhandled local player movement update");
     }
+
+    /**
+     * Handle the movement/placement of other (already tracked) players.
+     * 
+     * This method is pulled from the original implementation with minimal changes.
+     * 
+     * @param update The update information to apply
+     */
+    private void handleOtherPlayersMovement(OtherPlayersMovementUpdate update) {
+        int trackedPlayerCount = update.playerCount;
+        if(trackedPlayerCount < Player.localPlayerCount) {
+            for(int i = trackedPlayerCount; Player.localPlayerCount > i; i++)
+                Player.deregisterActorIndices[Class17.deregisterActorCount++] = Player.trackedPlayerIndices[i];
+        }
+        
+        if(Player.localPlayerCount < trackedPlayerCount) {
+            System.out.println("gppov1 " + Player.localPlayerCount + " " + trackedPlayerCount);
+            throw new RuntimeException("gppov1");
+        }
+
+        Player.localPlayerCount = 0;
+
+        for(int i = 0; trackedPlayerCount > i; i++) {
+            int trackedPlayerIndex = Player.trackedPlayerIndices[i];
+            Player player = Player.trackedPlayers[trackedPlayerIndex];
+
+            OtherPlayerMovementUpdate playerUpdate = update.movementUpdates[i];
+            
+            // no update required
+            if (playerUpdate == null) {
+                Player.trackedPlayerIndices[Player.localPlayerCount++] = trackedPlayerIndex;
+                player.anInt3134 = MovedStatics.pulseCycle;
+                continue;
+            }
+
+            // deregister player if necessary
+            if (playerUpdate.shouldDeregister) {
+                Player.deregisterActorIndices[Class17.deregisterActorCount++] = trackedPlayerIndex;
+                continue;
+            }
+            
+            Player.trackedPlayerIndices[Player.localPlayerCount++] = trackedPlayerIndex;
+            player.anInt3134 = MovedStatics.pulseCycle;
+
+            // if no movement then we can skip the rest
+            if (playerUpdate.movementUpdate == null) {
+                Player.actorUpdatingIndices[Player.actorUpdatingIndex++] = trackedPlayerIndex;
+                continue;
+            }
+
+            // handle the player movement
+            PlayerMovementUpdate movementUpdate = playerUpdate.movementUpdate;
+
+            if (movementUpdate.runDirection != null) {
+                player.move(movementUpdate.walkDirection, true);
+                player.move(movementUpdate.runDirection, true);
+            } else {
+                player.move(movementUpdate.walkDirection, false);
+            }
+
+            if (movementUpdate.runUpdateBlock) {
+                Player.actorUpdatingIndices[Player.actorUpdatingIndex++] = trackedPlayerIndex;
+            }
+        }
+    }
+
 }
