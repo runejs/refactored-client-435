be6b579776b065edfab15edb2f607aaf9c4bcc85
James Monger <jameskmonger@hotmail.co.uk>
2023-02-28 18:57:51 +0000

    feat: handle local player movement/placement
    You can see the original implementation in
    `Player.parsePlayerMovement`, for reference.

--- src/main/java/org/runejs/client/message/handler/rs435/UpdatePlayersMessageHandler.java
+++ src/main/java/org/runejs/client/message/handler/rs435/UpdatePlayersMessageHandler.java
@@ -2,7 +2,9 @@ package org.runejs.client.message.handler.rs435;
 
 import org.runejs.client.Class17;
 import org.runejs.client.media.renderable.actor.Actor;
+import org.runejs.client.media.renderable.actor.Player;
 import org.runejs.client.message.handler.MessageHandler;
+import org.runejs.client.message.inbound.updating.LocalPlayerMovementUpdate;
 import org.runejs.client.message.inbound.updating.UpdatePlayersInboundMessage;
 
 /**
@@ -18,8 +20,10 @@ public class UpdatePlayersMessageHandler implements MessageHandler<UpdatePlayers
         // initialisation steps for player updating
         Actor.actorUpdatingIndex = 0;
         Class17.deregisterActorCount = 0;
+        
+        // handle the different types of update within the message
+        handleLocalPlayerMovement(message.localPlayerMovement);
 
-        // TODO handle message here
         // handle any deregistrations
         for(int i = 0; Class17.deregisterActorCount > i; i++) {
             int trackedPlayerIndex = Player.deregisterActorIndices[i];
@@ -33,5 +37,58 @@ public class UpdatePlayersMessageHandler implements MessageHandler<UpdatePlayers
             if(Player.trackedPlayers[Player.trackedPlayerIndices[i]] == null)
                 throw new RuntimeException("gpp2 pos:" + i + " size:" + Player.localPlayerCount);
         }
+    }
+
+    /**
+     * Responsible for applying movement to the client's local player.
+     * 
+     * @param update The update method
+     */
+    private void handleLocalPlayerMovement(LocalPlayerMovementUpdate update) {
+        // if there is no update at all we do nothing
+        if (update == null) {
+            return;
+        }
+
+        if (update.mapRegionUpdate == null && update.movementUpdate == null) {
+            // "no movement" is handled differently to "no update", and needs this to be set
+            // this is indicated by mapRegionUpdate and movementUpdate both being null
+            Player.actorUpdatingIndices[Player.actorUpdatingIndex++] = 2047;
+            return;
+        }
+
+        // map region updates
+        if (update.mapRegionUpdate != null) {
+            Player.worldLevel = update.mapRegionUpdate.level;
+
+            if (update.mapRegionUpdate.runUpdateBlock) {
+                Player.actorUpdatingIndices[Player.actorUpdatingIndex++] = 2047;
+            }
+            
+            Player.localPlayer.method787(update.mapRegionUpdate.localChunkY, -7717, update.mapRegionUpdate.teleporting, update.mapRegionUpdate.localChunkX);
+
+            return;
+        }
+
+        // general movement updates (walking etc)
+        if (update.movementUpdate != null) {
+            PlayerMovementUpdate playerMovementUpdate = update.movementUpdate;
+
+            if (playerMovementUpdate.runDirection != null) {
+                Player.localPlayer.move(playerMovementUpdate.walkDirection, true);
+                Player.localPlayer.move(playerMovementUpdate.runDirection, true);
+            } else {
+                Player.localPlayer.move(playerMovementUpdate.walkDirection, false);
+            }
+
+            if (playerMovementUpdate.runUpdateBlock) {
+                Player.actorUpdatingIndices[Player.actorUpdatingIndex++] = 2047;
+            }
+
+            return;
+        }
+
+        // code should never reach here based on if statements above
+        throw new RuntimeException("Unhandled local player movement update");
     }
 }
