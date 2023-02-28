d2e01da6318e77282e7b513b35de906faf8a8f29
James Monger <jameskmonger@hotmail.co.uk>
2023-02-28 18:54:12 +0000

    feat: set up handler with initialisation/cleanup steps

--- src/main/java/org/runejs/client/message/handler/rs435/RS435HandlerRegistry.java
+++ src/main/java/org/runejs/client/message/handler/rs435/RS435HandlerRegistry.java
@@ -1,11 +1,15 @@
 package org.runejs.client.message.handler.rs435;
 
 import org.runejs.client.message.handler.MessageHandlerRegistry;
+import org.runejs.client.message.inbound.updating.UpdatePlayersInboundMessage;
 
 /**
  * A {@link MessageHandlerRegistry} for the RS revision 435 client.
  */
 public class RS435HandlerRegistry extends MessageHandlerRegistry {
     public RS435HandlerRegistry() {
+        super();
+
+        register(UpdatePlayersInboundMessage.class, new UpdatePlayersMessageHandler());
     }
 }
