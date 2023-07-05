c29de57e29494d1e7c8d4ec76a1d46cbd355548b
James Monger <jameskmonger@hotmail.co.uk>
2023-07-05 14:24:42 +0100

    fix: remove accidental commit

--- src/main/java/org/runejs/client/frame/console/Commands/EchoCommand.java
+++ src/main/java/org/runejs/client/frame/console/Commands/EchoCommand.java
@@ -1,6 +1,5 @@
 package org.runejs.client.frame.console.Commands;
 
-import org.runejs.client.MovedStatics;
 import org.runejs.client.RSString;
 import org.runejs.client.frame.console.Command;
 import org.runejs.client.frame.console.Console;
@@ -13,9 +12,14 @@ public class EchoCommand extends Command {
 
     @Override
     public void execute(Console console, String[] cmdInput) {
-        int x = Integer.parseInt(cmdInput[1]);
-
-        MovedStatics.anInt854 = x;
+        StringBuilder torepeat = new StringBuilder();
+        for (int i = 1; i < cmdInput.length; i++) {
+            if (i > 1) {
+                torepeat.append(" ");
+            }
+            torepeat.append(cmdInput[i]);
+        }
+        console.log("\"" + RSString.CreateString(torepeat.toString()).toString() + "\"");
 
 //        printConsoleMessage("<col=00FF00>search [item | npc | object] [name]</col> - <col=FFFF00>Returns list of results</col>", false);
     }
