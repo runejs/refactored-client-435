2b672498e23f5cf28b13dda34314ab03519a789e
James Monger <jameskmonger@hotmail.co.uk>
2023-07-10 19:00:20 +0100

    feat: show path coords on debugwalk

--- src/main/java/org/runejs/client/frame/DebugTools.java
+++ src/main/java/org/runejs/client/frame/DebugTools.java
@@ -1,6 +1,7 @@
 package org.runejs.client.frame;
 
 import org.runejs.client.MovedStatics;
+import org.runejs.client.cache.media.TypeFace;
 import org.runejs.client.media.Rasterizer;
 import org.runejs.client.scene.Point2d;
 
@@ -25,10 +26,16 @@ public class DebugTools {
             int tileX = walkpathX[i];
             int tileY = walkpathY[i];
 
-            Point2d nextPos = MovedStatics.getProjectedScreenPosition(10, tileY * 128 + 64, tileX * 128 + 64);
+            Point2d nextPos = MovedStatics.getProjectedScreenPosition(0, tileY * 128 + 64, tileX * 128 + 64);
 
             if (lastTilePos != null && nextPos != null) {
                 Rasterizer.drawDiagonalLine(lastTilePos.x, lastTilePos.y, nextPos.x, nextPos.y, 0x00FFAC);
+            }
+
+            // handle final walkpath label separately to avoid clipping issues
+            if (nextPos != null && i != walkpathX.length - 1) {
+                TypeFace.fontSmall.drawStringLeft(tileX + "," + tileY, nextPos.x, nextPos.y + 1, 0);
+                TypeFace.fontSmall.drawStringLeft(tileX + "," + tileY, nextPos.x, nextPos.y, 0x00AAFF);
             }
 
             lastTileX = tileX;
@@ -38,10 +45,14 @@ public class DebugTools {
 
         if (pathStartPos != null) {
             Rasterizer.drawCircle(pathStartPos.x, pathStartPos.y, 2, 0xE055DE);
+            TypeFace.fontSmall.drawStringLeft(walkpathX[0] + "," + walkpathY[0], pathStartPos.x, pathStartPos.y + 1, 0);
+            TypeFace.fontSmall.drawStringLeft(walkpathX[0] + "," + walkpathY[0], pathStartPos.x, pathStartPos.y, 0x00AAFF);
         }
 
         if (pathFinishPos != null) {
             Rasterizer.drawCircle(pathFinishPos.x, pathFinishPos.y, 4, 0xE055DE);
+            TypeFace.fontSmall.drawStringLeft(walkpathX[walkpathX.length - 1] + "," + walkpathY[walkpathY.length - 1], pathFinishPos.x, pathFinishPos.y + 1, 0);
+            TypeFace.fontSmall.drawStringLeft(walkpathX[walkpathX.length - 1] + "," + walkpathY[walkpathY.length - 1], pathFinishPos.x, pathFinishPos.y, 0x00AAFF);
         }
     }
 }
