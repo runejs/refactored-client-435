7a428a3aeeb19f8bc3f2be0365746eb9ac6caf10
James Monger <jameskmonger@hotmail.co.uk>
2023-07-12 00:30:30 +0100

    refactor: move parseException to MovedStatics

--- src/main/java/org/runejs/client/Class55.java
+++ src/main/java/org/runejs/client/Class55.java
@@ -1,50 +1,8 @@
 package org.runejs.client;
 
-import java.io.*;
-
 public class Class55 {
     public static volatile int eventClickX = 0;
     public static int destinationY = 0;
-
-    public static String parseException(Throwable exception) throws IOException {
-        String string;
-        if(exception instanceof RSRuntimeException) {
-            RSRuntimeException runtimeexception_sub1 = (RSRuntimeException) exception;
-            string = runtimeexception_sub1.aString1653 + " | ";
-            exception = runtimeexception_sub1.aThrowable1652;
-        } else
-            string = "";
-        StringWriter stringwriter = new StringWriter();
-        PrintWriter printwriter = new PrintWriter(stringwriter);
-        exception.printStackTrace(printwriter);
-        printwriter.close();
-        String string_0_ = stringwriter.toString();
-        BufferedReader bufferedreader = new BufferedReader(new StringReader(string_0_));
-        String string_1_ = bufferedreader.readLine();
-        for(; ; ) {
-            String string_2_ = bufferedreader.readLine();
-            if(string_2_ == null)
-                break;
-            int i = string_2_.indexOf('(');
-            int i_3_ = string_2_.indexOf(')', i + 1);
-            if(i >= 0 && i_3_ >= 0) {
-                String string_4_ = string_2_.substring(1 + i, i_3_);
-                int i_5_ = string_4_.indexOf(".java:");
-                if(i_5_ >= 0) {
-                    string_4_ = string_4_.substring(0, i_5_) + string_4_.substring(5 + i_5_);
-                    string += string_4_ + ' ';
-                    continue;
-                }
-                string_2_ = string_2_.substring(0, i);
-            }
-            string_2_ = string_2_.trim();
-            string_2_ = string_2_.substring(1 + string_2_.lastIndexOf(' '));
-            string_2_ = string_2_.substring(1 + string_2_.lastIndexOf('\t'));
-            string += string_2_ + ' ';
-        }
-        string += "| " + string_1_;
-        return string;
-    }
 
 
 }
--- src/main/java/org/runejs/client/MovedStatics.java
+++ src/main/java/org/runejs/client/MovedStatics.java
@@ -44,7 +44,7 @@ import org.runejs.client.media.renderable.actor.*;
 import org.runejs.Configuration;
 
 import java.awt.*;
-import java.io.DataInputStream;
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
@@ -565,7 +565,7 @@ public class MovedStatics {
             try {
                 String string = "";
                 if (exception != null)
-                    string = Class55.parseException(exception);
+                    string = parseException(exception);
                 if (arg0 != null) {
                     if (exception != null)
                         string += " | ";
@@ -2755,5 +2755,45 @@ public class MovedStatics {
         } catch(Exception exception) {
             MouseHandler.gameCanvas.repaint();
         }
+    }
+
+    public static String parseException(Throwable exception) throws IOException {
+        String string;
+        if(exception instanceof RSRuntimeException) {
+            RSRuntimeException runtimeexception_sub1 = (RSRuntimeException) exception;
+            string = runtimeexception_sub1.aString1653 + " | ";
+            exception = runtimeexception_sub1.aThrowable1652;
+        } else
+            string = "";
+        StringWriter stringwriter = new StringWriter();
+        PrintWriter printwriter = new PrintWriter(stringwriter);
+        exception.printStackTrace(printwriter);
+        printwriter.close();
+        String string_0_ = stringwriter.toString();
+        BufferedReader bufferedreader = new BufferedReader(new StringReader(string_0_));
+        String string_1_ = bufferedreader.readLine();
+        for(; ; ) {
+            String string_2_ = bufferedreader.readLine();
+            if(string_2_ == null)
+                break;
+            int i = string_2_.indexOf('(');
+            int i_3_ = string_2_.indexOf(')', i + 1);
+            if(i >= 0 && i_3_ >= 0) {
+                String string_4_ = string_2_.substring(1 + i, i_3_);
+                int i_5_ = string_4_.indexOf(".java:");
+                if(i_5_ >= 0) {
+                    string_4_ = string_4_.substring(0, i_5_) + string_4_.substring(5 + i_5_);
+                    string += string_4_ + ' ';
+                    continue;
+                }
+                string_2_ = string_2_.substring(0, i);
+            }
+            string_2_ = string_2_.trim();
+            string_2_ = string_2_.substring(1 + string_2_.lastIndexOf(' '));
+            string_2_ = string_2_.substring(1 + string_2_.lastIndexOf('\t'));
+            string += string_2_ + ' ';
+        }
+        string += "| " + string_1_;
+        return string;
     }
 }
