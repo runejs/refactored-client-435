9c6e6bbfec45d228bd0f10527b0b89031abf020b
James Monger <jameskmonger@hotmail.co.uk>
2023-07-13 12:57:38 +0100

    refactor: create FrameRenderer interface, convert FrameRenderer to ResizableFrameRenderer

--- src/main/java/org/runejs/client/frame/FrameRenderer.java
+++ src/main/java/org/runejs/client/frame/FrameRenderer.java
@@ -1,34 +1,4 @@
 package org.runejs.client.frame;
 
-import org.runejs.client.Game;
-import org.runejs.client.frame.tab.TabProducer;
-
-public class FrameRenderer implements Runnable {
-    private final Minimap minimap;
-    private final TabProducer tabProducer;
-
-    public FrameRenderer(Minimap minimap, TabProducer tabProducer) {
-        this.minimap = minimap;
-        this.tabProducer = tabProducer;
-    }
-
-    @Override
-    public void run() {
-        while(true){
-//            System.out.println("Rendering");
-
-            if(Game.gameStatusCode <= 35 && Game.gameStatusCode >= 30){
-                minimap.RenderResizableMiniMapArea(ScreenController.drawWidth - 210, 0);
-            }
-            if(Game.gameStatusCode <= 35 && Game.gameStatusCode >= 30){
-                tabProducer.RenderResizableSideBarArea();
-            }
-            try {
-                Thread.sleep(1);
-            } catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
+public interface FrameRenderer extends Runnable {
 }
